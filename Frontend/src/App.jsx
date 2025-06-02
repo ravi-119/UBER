e2e4bed215214a18f1ae428ae9d4df6b12e544c4
ravi-119 <ry277015@gmail.com>
2025-06-03 01:18:01 +0530

    Authentication is done

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import UserLogin from './pages/UserLogin'
@@ -7,6 +7,7 @@ import Captainlogin from './pages/Captainlogin'
 import CaptainSignup from './pages/CaptainSignup'
 
 const App = () => {
+  
   return (
     <div>
         <Routes>
--- Frontend/src/context/userContext.jsx
+++ Frontend/src/context/userContext.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { createContext, useContext, useState } from 'react';
 
-const userContext = () => {
+export const UserDataContext = createContext();
+
+export const useUserData = () => useContext(UserDataContext);
+
+const UserContextProvider = ({ children }) => {
+  const [user, setUser] = useState({
+    email: '',
+    fullname: { // changed to match backend
+      firstname: '',
+      lastname: ''
+    }
+  });
+
   return (
-    <div>userContext</div>
-  )
-}
+    <UserDataContext.Provider value={{ user, setUser }}>
+      {children}
+    </UserDataContext.Provider>
+  );
+};
 
-export default userContext
+export default UserContextProvider;
